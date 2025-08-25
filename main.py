a3bbe2140e7e8303d55ec15dc8e66ebbab005feb
Dieart38 <diegoarts@protonmail.com>
2025-08-25 09:03:50 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+pygame.init()
+# Criando a janela de jogo
+print("Setup Start")
+window = pygame.display.set_mode(size = (800, 600)) # passa o tamanho da tela do jogo
+clock = pygame.time.Clock()
+print("Setup End")
+
+print("Loop Start")
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # end pygame
